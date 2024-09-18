bceae472c0225f3454990e36a3a15ea547c2bc43
abuba0a <168736244+abuba0a@users.noreply.github.com>
2024-09-18 08:28:57 +0300

    Add files via upload

---
+++ module_12_2.py
@@ -0,0 +1,47 @@
+# -*- coding: utf8 -*-
+import HumanMoveTest
+from unittest import TestCase
+
+
+class TournamentTest(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.runner_1 = HumanMoveTest.Runner('Усэйн', 10)
+        self.runner_2 = HumanMoveTest.Runner('Андрей', 9)
+        self.runner_3 = HumanMoveTest.Runner('Ник', 3)
+
+    @classmethod
+    def tearDownClass(cls):
+        for test_key, test_value in cls.all_results.items():
+            key_position = 1
+            for key, value in test_value.items():
+                print(f'{key_position} : {value.name}')
+                key_position += 1
+
+    def test_turn_1(self):
+        turn_1 = HumanMoveTest.Tournament(90, self.runner_1, self.runner_3)
+        result = turn_1.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник')
+        self.all_results['1,2'] = result
+        print(f'{result}')
+
+    def test_turn_2(self):
+        turn_2 = HumanMoveTest.Tournament(90, self.runner_2, self.runner_3)
+        result = turn_2.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник')
+        self.all_results['2,3'] = result
+        print(f'{result}')
+
+    def test_turn_3(self):
+        turn_3 = HumanMoveTest.Tournament(90, self.runner_1, self.runner_2, self.runner_3)
+        result = turn_3.start()
+        self.assertTrue(result[list(result.keys())[-1]] == 'Ник')
+        self.all_results['1,2,3'] = result
+        print(f'{result}')
+
+
+TournamentTest()
